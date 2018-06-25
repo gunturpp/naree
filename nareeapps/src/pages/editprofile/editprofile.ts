4a37bdceb0ef3b50d11d34496af3c349498fe816
thirafi <thirafi_aja@apps.ipb.ac.id>
2018-06-25 15:02:24 +0700

    revisi 4

--- nareeapps/src/pages/editprofile/editprofile.ts
+++ nareeapps/src/pages/editprofile/editprofile.ts
@@ -32,11 +32,14 @@ export class EditprofilePage {
   hp:any;
   about:any;
   achiev:any;
+  prov:any;
   usrname:any;
   users: any;
   user:any;
   // user: { name?: string, email?: string, password?: string, gender?: string, birthdate?: string, occupation?: string } = {};
   c_password: string;
+  ProvinceData: { text: string; value: string; }[];
+  province: { text: string; value: string; };
   constructor(public http: Http,
     public navCtrl: NavController,
     public authService: AuthServiceProvider,
@@ -45,7 +48,46 @@ export class EditprofilePage {
     public alertCtrl: AlertController,
     public actionSheetCtrl: ActionSheetController) {
     this.profiles = JSON.parse(localStorage.getItem('currentUser'));
+    this.ProvinceData = [
+      { text: 'Aceh', value: 'Aceh' },
+      { text: 'Bali', value: 'Bali' },
+      { text: 'Banten', value: 'Banten' },
+      { text: 'Bengkulu', value: 'Bengkulu' },
+      { text: 'Gorontalo', value: 'Gorontalo' },
+      { text: 'Jakarta', value: 'Jakarta' },
+      { text: 'Jambi', value: 'Jambi' },
+      { text: 'Jawa Barat', value: 'Jawa Barat' },
+      { text: 'Jawa Tengah', value: 'Jawa Tengah' },
+      { text: 'Jawa Timur', value: 'Jawa Timur' },
+      { text: 'Kalimantan Barat', value: 'Kalimantan Barat' },
+      { text: 'Kalimantan Selatan', value: 'Kalimantan Selatan' },
+      { text: 'Kalimantan Tengah', value: 'Kalimantan Tengah' },
+      { text: 'Kalimantan Timur', value: 'Kalimantan Timur' },
+      { text: 'Kalimantan Utara', value: 'Kalimantan Utara' },
+      { text: 'Kepulauan Bangka Belitung', value: 'Kepulauan Bangka Belitung' },
+      { text: 'Kepulauan Riau', value: 'Kepulauan Riau' },
+      { text: 'Lampung', value: 'Lampung' },
+      { text: 'Maluku', value: 'Maluku' },
+      { text: 'Maluku Utara', value: 'Maluku Utara' },
+      { text: 'Nusa Tenggara Barat', value: 'Nusa Tenggara Barat' },
+      { text: 'Nusa Tenggara Timur', value: 'Nusa Tenggara Timur' },
+      { text: 'Papua', value: 'Papua' },
+      { text: 'Papua Barat', value: 'Papua Barat' },
+      { text: 'Riau', value: 'Riau' },
+      { text: 'Sulawesi Barat', value: 'Sulawesi Barat' },
+      { text: 'Sulawesi Selatan', value: 'Sulawesi Selatan' },
+      { text: 'Sulawesi Tengah', value: 'Sulawesi Tengah' },
+      { text: 'Sulawesi Tenggara', value: 'Sulawesi Tenggara' },
+      { text: 'Sulawesi Utara', value: 'Sulawesi Utara' },
+      { text: 'Sumatera Barat', value: 'Sumatera Barat' },
+      { text: 'Sumatera Selatan', value: 'Sumatera Selatan' },
+      { text: 'Sumatera Utara', value: 'Sumatera Utara' },
+      { text: 'Yogyakarta', value: 'Yogyakarta' }
+  ];
   }
+  compareFn(option1: any, option2: any) {
+    return option1.value === option2.value;
+}
   ionViewDidLoad() {
     this.http.get("https://nareeapp.com/api/users/"+this.profiles.id +"/edit").subscribe( userss => {
       let response = userss.json();
@@ -60,9 +102,11 @@ export class EditprofilePage {
       this.email = this.user.email;
       this.hp = this.user.no_hp;
       this.about = this.user.about_me;
+      this.prov = this.user.province;
       this.iduser= this.user.id;
       // console.log("ini hasilnya" + JSON.stringify(this.user));
-      console.log("user profile" ,this.user);
+      console.log("user profile" ,this.prov);
+      this.province = { text: this.prov, value: this.prov };
       if(response.status=="200"){
         this.users= response.data;   //ini disimpen ke variabel pasien diatas itu ,, yang udah di delacre
       }
@@ -103,6 +147,7 @@ export class EditprofilePage {
           username:this.profiles.username,
           gender:this.gender,
           birthdate:this.tgl,
+          province:this.province.value,
           occupation: this.profiles.occupation,
           photo:this.profiles.photo,
           no_hp :this.hp,
@@ -119,7 +164,7 @@ export class EditprofilePage {
           // this.navCtrl.push(ProfilPasien);
           let alert = this.alertCtrl.create({
           title: 'Data Tersimpan!',
-          subTitle: 'Lakukan refresh dengan cara menarik halaman kebawah',
+          subTitle: '',
           buttons: ['OK']
           });
           alert.present();
